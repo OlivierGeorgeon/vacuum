d7fb6ddc97863f7e588843c5151a90a766c13420
simon.gay@alumni.enseeiht.fr <simon.gay@alumni.enseeiht.fr@22465167-65bd-f978-0e0a-f6e8ab375494>
2011-10-04 13:12:06 +0000

    local space map implemented tactile sensor mapping deactivated

--- trunk/vacuumSG1/src/TactileMapPanel.java
+++ trunk/vacuumSG1/src/TactileMapPanel.java
@@ -11,79 +11,103 @@ public class TactileMapPanel extends JPanel{
 	 */
 	private static final long serialVersionUID = 1L;
 	TactileMap tmap;
+	public int width;
 	
 	
 	public TactileMapPanel(TactileMap t){
 		tmap=t;
+		width=200/(40-10);//tmap.mapSize;
 	}
 	
 	public void paintComponent(Graphics g){
 		g.setColor(Color.white);
-		g.fillRect(0, 0, 600, 600);
-
+		g.fillRect(0, 0, 800, 800);
 		
+		/*
 		// draw the tactile retina
-		int width=600/(tmap.resolution*tmap.sensorRes);
+		width=600/(tmap.resolution*tmap.sensorRes);
 		g.setColor(Color.black);
 		for (int i=0;i<tmap.resolution*tmap.sensorRes;i++){
 			g.fillRect(width*i, 110-(int)(tmap.m_tactilePressure[i]*100), width, (int)(tmap.m_tactilePressure[i]*100) );
 		}
+		*/
 		
 		
-		
-		// draw the potential map
-		g.setColor(Color.black);
-		g.drawRect(5, 345, 290, 210);
-		
-		width=200/(40-10);//tmap.mapSize;
-		int offsetX,offsetY;
-		for (int k=0;k<Math.min(3, tmap.flowX1.size());k++){
-			for (int i=10;i<40;i++){
-				for (int j=10;j<40;j++){
-				
-					// potential map
-					//g.setColor(new Color(Math.min(1,tmap.potentialMap[i][j]),1-Math.min(1,tmap.potentialMap[i][j]),0));
-					//g.fillRect(450-tmap.mapSize/2*width + i*width, 450-tmap.mapSize/2*width + j*width, width, width);
 
-					// draw flow
-					g.setColor(new Color(Math.min(1,tmap.potentialMap[i][j]),1-Math.min(1,tmap.potentialMap[i][j]),0));
-				
-				
+		
+		
+		int offsetX,offsetY;
+		
+		
+		
+		for (int i=0;i<50;i++){
+			for (int j=0;j<50;j++){
+			
+				// potential map
+				//g.setColor(new Color(Math.min(1,tmap.potentialMap[i][j]),1-Math.min(1,tmap.potentialMap[i][j]),0));
+				//g.fillRect(450-tmap.mapSize/2*width + i*width, 450-tmap.mapSize/2*width + j*width, width, width);
+
+				// charge map
+				g.setColor(new Color(Math.min(1,tmap.chargeMap0[i][j][0]),
+						             Math.min(1,tmap.chargeMap0[i][j][1]),
+						             1-Math.min(1,Math.max(tmap.chargeMap0[i][j][0],tmap.chargeMap0[i][j][1]))) );
+				g.fillRect(550-tmap.mapSize/2*width + i*width, 150-tmap.mapSize/2*width + j*width, width, width);
+			}
+		}
+		
+		
+		for (int k=0;k<Math.min(3, tmap.flowX1.size());k++){
+			
+			for (int i=0;i<50;i+=2){
+				for (int j=0;j<50;j+=2){
+
+					
 					// draw improved flow
-					offsetX=0;
+					offsetX=50;
 					offsetY=0;
 					if (k==0){
-						offsetX=450;
-						offsetY=450;
+						offsetX=550;
+						offsetY=550;
 					}
 					else if (k==1){
-						offsetX=150;
-						offsetY=450;
+						offsetX=200;
+						offsetY=550;
 					}
 					else if (k==2){
-						offsetX=450;
-						offsetY=250;
+						offsetX=550;
+						offsetY=150;
 					}
 					
-					if (tmap.flowX1.size()>0 && tmap.flowX1.get(k)[i][j]!=0 || tmap.flowY1.get(k)[i][j]!=0)
-						g.drawLine(offsetX-tmap.mapSize/2*width + i*width+width/2,offsetY-tmap.mapSize/2*width + j*width+width/2,
-								   offsetX-tmap.mapSize/2*width + i*width+width/2+(int)(10*tmap.flowX2.get(k)[i][j]),
-								   offsetY-tmap.mapSize/2*width + j*width+width/2+(int)(10*tmap.flowY2.get(k)[i][j]) );
-				
+					
+					if (tmap.testMap[i][j]){
+						g.setColor(new Color(Math.min(1,tmap.potentialMap[i][j]),1-Math.min(1,tmap.potentialMap[i][j]),0));
+					
+						g.drawLine(offsetX-tmap.mapSize/2*width + i*width+width/2,
+								   offsetY-tmap.mapSize/2*width + j*width+width/2,
+								   offsetX-tmap.mapSize/2*width + i*width+width/2+(int)(1000*tmap.flowX2.get(k)[i][j]),
+								   offsetY-tmap.mapSize/2*width + j*width+width/2+(int)(1000*tmap.flowY2.get(k)[i][j]) );
+					}
+					else{
+						g.setColor(Color.blue);
+						g.drawLine(offsetX-tmap.mapSize/2*width + i*width+width/2,
+								   offsetY-tmap.mapSize/2*width + j*width+width/2,
+								   offsetX-tmap.mapSize/2*width + i*width+width/2+(int)(1000*tmap.flowX2.get(k)[i][j]),
+								   offsetY-tmap.mapSize/2*width + j*width+width/2+(int)(1000*tmap.flowY2.get(k)[i][j]) );
+					}
 				}
 			}
 		}
 		
 		
-		// draw sensor neurons in the internal map
+		// draw sensor neuron activity in the internal map
 		g.setColor(Color.black);
-		g.drawRect(5, 150, 290, 195);		
+		g.drawRect(5, 50, 290, 195);		
 		for (int i=0;i<tmap.resolution*tmap.sensorRes;i++){
 			g.setColor(tmap.m_tactileObject[i]);
-			g.fillOval(150+(int)(tmap.sensorX[i]-25*tmap.m_tactilePressure[i]), 250+(int)(tmap.sensorY[i]-25*tmap.m_tactilePressure[i]),
+			g.fillOval(150+(int)(tmap.sensorX[i]-25*tmap.m_tactilePressure[i]), 150+(int)(tmap.sensorY[i]-25*tmap.m_tactilePressure[i]),
 					   (int)(50*tmap.m_tactilePressure[i]), (int)(50*tmap.m_tactilePressure[i]));
 		}
-		
+
 		
 		for (int i=0;i<tmap.resolution*tmap.sensorRes;i++){
 			// draw neurons in neuron display
@@ -92,35 +116,93 @@ public class TactileMapPanel extends JPanel{
 			else if (tmap.m_tactileVariations[i]==-20) g.setColor(Color.blue );
 			else g.setColor(Color.black);
 			
-			// neurons
-			g.fillOval(150+(int)(tmap.sensorX[i])-2, 250+(int)(tmap.sensorY[i])-2, 4, 4);
+			// neurons in cartesian display
+			g.fillOval(150+(int)(tmap.sensorX[i])-2, 150+(int)(tmap.sensorY[i])-2, 4, 4);
 			
 			// connections of first neuron layer
 			g.setColor(Color.black);
-			if (i< tmap.resolution-1) g.drawLine(150+(int)tmap.sensorX[i  ], 250+(int)tmap.sensorY[i  ],
-					                             150+(int)tmap.sensorX[i+1], 250+(int)tmap.sensorY[i+1]);
-			if (i==tmap.resolution-1) g.drawLine(150+(int)tmap.sensorX[i  ], 250+(int)tmap.sensorY[i  ],
-                                                 150+(int)tmap.sensorX[0  ], 250+(int)tmap.sensorY[0  ]);
+			if (i< tmap.resolution-1) g.drawLine(150+(int)tmap.sensorX[i  ], 150+(int)tmap.sensorY[i  ],
+					                             150+(int)tmap.sensorX[i+1], 150+(int)tmap.sensorY[i+1]);
+			if (i==tmap.resolution-1) g.drawLine(150+(int)tmap.sensorX[i  ], 150+(int)tmap.sensorY[i  ],
+                                                 150+(int)tmap.sensorX[0  ], 150+(int)tmap.sensorY[0  ]);
 			
 			// connections of second neuron layer
 			g.setColor(Color.LIGHT_GRAY);
-			if (i>=tmap.resolution && i<2*tmap.resolution-1) g.drawLine(150+(int)tmap.sensorX[i  ], 250+(int)tmap.sensorY[i  ],
-                    													150+(int)tmap.sensorX[i+1], 250+(int)tmap.sensorY[i+1]);
-			if (i==2*tmap.resolution-1) g.drawLine(150+(int)tmap.sensorX[i  ], 250+(int)tmap.sensorY[i  ],
-												   150+(int)tmap.sensorX[tmap.resolution], 250+(int)tmap.sensorY[tmap.resolution]);
+			if (i>=tmap.resolution && i<2*tmap.resolution-1) g.drawLine(150+(int)tmap.sensorX[i  ], 150+(int)tmap.sensorY[i  ],
+                    													150+(int)tmap.sensorX[i+1], 150+(int)tmap.sensorY[i+1]);
+			if (i==2*tmap.resolution-1) g.drawLine(150+(int)tmap.sensorX[i  ], 150+(int)tmap.sensorY[i  ],
+												   150+(int)tmap.sensorX[tmap.resolution], 150+(int)tmap.sensorY[tmap.resolution]);
 			
 			// connections between first and second layer
 			g.setColor(Color.black);
-			if (i<tmap.resolution && tmap.sensorRes>1) g.drawLine(150+(int)tmap.sensorX[i  ], 250+(int)tmap.sensorY[i  ],
-                                                150+(int)tmap.sensorX[i+tmap.resolution], 250+(int)tmap.sensorY[i+tmap.resolution]);
+			if (i<tmap.resolution && tmap.sensorRes>1) g.drawLine(150+(int)tmap.sensorX[i  ], 150+(int)tmap.sensorY[i  ],
+                                                150+(int)tmap.sensorX[i+tmap.resolution], 150+(int)tmap.sensorY[i+tmap.resolution]);
 			
 			// connections between second and third layer
 			if (i>=tmap.resolution && i<2*tmap.resolution && tmap.sensorRes>2){
-				    g.drawLine(150+(int)tmap.sensorX[i], 250+(int)tmap.sensorY[i],
-                    150+(int)tmap.sensorX[i+tmap.resolution], 250+(int)tmap.sensorY[i+tmap.resolution]);
+				    g.drawLine(150+(int)tmap.sensorX[i], 150+(int)tmap.sensorY[i],
+                    150+(int)tmap.sensorX[i+tmap.resolution], 150+(int)tmap.sensorY[i+tmap.resolution]);
 			}
 
 		}
+		
+		/*
+		// draw flow lines
+		g.setColor(Color.black);
+		for (int k=0;k<Math.min(3, tmap.flowX1.size());k++){
+			offsetX=0;
+			offsetY=0;
+			if (k==0){
+				offsetX=450;
+				offsetY=450;
+			}
+			else if (k==1){
+				offsetX=150;
+				offsetY=450;
+			}
+			else if (k==2){
+				offsetX=450;
+				offsetY=250;
+			}
+			
+			// real flow line
+			for (int i=5;i<45;i+=2){
+				for (int j=5;j<45;j+=2){
+					if (tmap.flowLineX1.get(k)[i][j][0]!=0 || tmap.flowLineY1.get(k)[i][j][0]!=0){
+						g.setColor(Color.black);
+						int n=0;
+						boolean test=true;
+						while (n<tmap.flowLength-1 && test){
+							g.drawLine(offsetX-tmap.mapSize/2*width + (int)(tmap.flowLineX1.get(k)[i][j][n]*width+width/2),
+									   offsetY-tmap.mapSize/2*width + (int)(tmap.flowLineY1.get(k)[i][j][n]*width+width/2),
+									   offsetX-tmap.mapSize/2*width + (int)(tmap.flowLineX1.get(k)[i][j][n+1]*width+width/2),
+									   offsetY-tmap.mapSize/2*width + (int)(tmap.flowLineY1.get(k)[i][j][n+1]*width+width/2) );
+							
+							n++;
+							if (n<tmap.flowLength-1)
+								if (tmap.flowLineX1.get(k)[i][j][n+1]==0 && tmap.flowLineY1.get(k)[i][j][n+1]==0) test=false;
+						}
+					}
+					
+					// extrapolated flow lines
+					else if (tmap.flowLineX2.get(k)[i][j][0]!=0 || tmap.flowLineY2.get(k)[i][j][0]!=0){
+						g.setColor(Color.blue);
+						int n=0;
+						boolean test=true;
+						while (n<tmap.flowLength-1 && test){
+							g.drawLine(offsetX-tmap.mapSize/2*width + (int)(tmap.flowLineX2.get(k)[i][j][n]*width+width/2),
+									   offsetY-tmap.mapSize/2*width + (int)(tmap.flowLineY2.get(k)[i][j][n]*width+width/2),
+									   offsetX-tmap.mapSize/2*width + (int)(tmap.flowLineX2.get(k)[i][j][n+1]*width+width/2),
+									   offsetY-tmap.mapSize/2*width + (int)(tmap.flowLineY2.get(k)[i][j][n+1]*width+width/2) );
+							
+							n++;
+							if (tmap.flowLineX2.get(k)[i][j][n+1]==0 && tmap.flowLineY2.get(k)[i][j][n+1]==0) test=false;
+						}
+					}
+
+				}
+			}
+		}*/
 
 	}
 	
