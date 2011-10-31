b45ca525eb0d335e538988db54b388ea53bbbdf5
simon.gay@alumni.enseeiht.fr <simon.gay@alumni.enseeiht.fr@22465167-65bd-f978-0e0a-f6e8ab375494>
2011-10-31 13:30:45 +0000

    functional colliculus without learning function

--- trunk/vacuumSG1/src/VisualMapPanel.java
+++ trunk/vacuumSG1/src/VisualMapPanel.java
@@ -25,15 +25,24 @@ public class VisualMapPanel extends JPanel{
 		
 		int offsetX,offsetY;
 		
+		
 		for (int i=0;i<180;i++){
 			for (int j=0;j<70;j++){
 				// draw charge map
-				//g.setColor(new Color(Math.min(1,vmap.potentialMap[i][j]),1-Math.min(1,vmap.potentialMap[i][j]),0));
-				g.setColor(new Color(vmap.timerMap[i][j]*5,(40-vmap.timerMap[i][j])*5,0));
+				g.setColor(new Color(Math.min(1,vmap.potentialMap[i][j]),1-Math.min(1,vmap.potentialMap[i][j]),0));
+				//g.setColor(new Color((int)vmap.timerMap[i][j]*5,(20-(int)vmap.timerMap[i][j])*5,0));
 				g.fillRect(10+ i*width, 10+ (70-j)*width, width*2, width*2);
 			}
-		}
+		}/* */
 		
+		/*
+		for (int i=0;i<100;i++){
+			for (int j=0;j<100;j++){
+				//g.setColor(new Color(Math.min(1,vmap.potentialCMap[i][j]),1-Math.min(1,vmap.potentialCMap[i][j]),0));
+				g.setColor(new Color(vmap.timerCMap[i][j]*5,(40-vmap.timerCMap[i][j])*5,0));
+				g.fillRect(10+ i*width, 10+ (100-j)*width, width, width);
+			}
+		}/* */
 		
 		for (int k=0;k<Math.min(3, vmap.flowX1.size());k++){
 			offsetX=50;
@@ -63,36 +72,36 @@ public class VisualMapPanel extends JPanel{
 							   offsetX-vmap.mapSize/2*width + i*width+width+(int)(1000*vmap.flowX3.get(k)[i][j]),
 							   offsetY-vmap.mapSize/2*width + j*width+width/2+(int)(1000*vmap.flowY3.get(k)[i][j]) );
 				}
-			}*/
+			}/* */
 			
-			/*
-			for (int i=0;i<180;i+=6){
-				for (int j=0;j<70;j+=6){
-					int lenght=40;
-					if (k>0) lenght=100;
+			
+			for (int i=0;i<180;i+=5){
+				for (int j=0;j<100;j+=5){
+					int lenght=100;
+					if (k>0) lenght=500;
 					// draw improved flow
 
 					//g.setColor(new Color(Math.min(1,tmap.potentialMap[i][j]),1-Math.min(1,tmap.potentialMap[i][j]),0));
 					g.setColor(Color.blue);
 					g.drawLine(offsetX-vmap.mapSize/2*width + i*width/2+width,
-							   offsetY-vmap.mapSize/2*width + (70-j)*width+width/2,
+							   offsetY-vmap.mapSize/2*width + j*width+width,
 							   offsetX-vmap.mapSize/2*width + i*width/2+width+(int)(lenght*vmap.flowX2.get(k)[i][j]),
-							   offsetY-vmap.mapSize/2*width + (70-j)*width+width/2+(int)(lenght*vmap.flowY2.get(k)[i][j]) );
+							   offsetY-vmap.mapSize/2*width + j*width+width+(int)(lenght*vmap.flowY2.get(k)[i][j]) );
 				}
 			}/* */
 			
-			
+			/*
 			for (int i=0;i<100;i+=4){
 				for (int j=0;j<100;j+=4){
-
-					// draw improved flow
+					int lenght=1000;
+					if (k>0) lenght=500;
 
 					//g.setColor(new Color(Math.min(1,tmap.potentialMap[i][j]),1-Math.min(1,tmap.potentialMap[i][j]),0));
 					g.setColor(Color.blue);
 					g.drawLine(offsetX-vmap.mapSize/2*width + i*width+width,
 							   offsetY-vmap.mapSize/2*width + j*width+width,
-							   offsetX-vmap.mapSize/2*width + i*width+width+(int)(500*vmap.flowX3.get(k)[i][j]),
-							   offsetY-vmap.mapSize/2*width + j*width+width+(int)(500*vmap.flowY3.get(k)[i][j]) );
+							   offsetX-vmap.mapSize/2*width + i*width+width+(int)(lenght*vmap.flowX3.get(k)[i][j]),
+							   offsetY-vmap.mapSize/2*width + j*width+width+(int)(lenght*vmap.flowY3.get(k)[i][j]) );
 				}
 			}/* */
 		}
